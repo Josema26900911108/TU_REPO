2cf6bd29e88895ca1e4f684dbb8a2418c64ee5fc
unknown <Josema@LAP-ING-F57GPL3.clarogt.americamovil.ca1>
2026-05-07 12:27:36 -0600

    Actualizacion rule compra

--- app/Http/Requests/StoreCompraRequest.php
+++ app/Http/Requests/StoreCompraRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StoreCompraRequest extends FormRequest
 {
@@ -19,15 +20,20 @@ class StoreCompraRequest extends FormRequest
      *
      * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
      */
-    public function rules(): array
-    {
-        return [
-            'proveedore_id' => 'required|exists:proveedores,id',
-            'comprobante_id' => 'required|exists:comprobantes,id',
-            'numero_comprobante' => 'required|unique:compras,numero_comprobante|max:255',
-            'impuesto' => 'required',
-            'fecha_hora' => 'required',
-            'total' => 'required'
-        ];
-    }
+public function rules(): array
+{
+    return [
+        'proveedore_id' => 'required|exists:proveedores,id',
+        'comprobante_id' => 'required|exists:comprobantes,id',
+        'numero_comprobante' => [
+            'required',
+            'max:255',
+            // Aplicamos la regla Unique pero con una excepción
+            Rule::unique('compras', 'numero_comprobante')->whereNot('numero_comprobante', '0')
+        ],
+        'impuesto' => 'required',
+        'fecha_hora' => 'required',
+        'total' => 'required'
+    ];
+}
 }
