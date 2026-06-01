8adb66e2bc642fbb66bec68eece9bd81adf1ca77
unknown <Josema@LAP-ING-F57GPL3.clarogt.americamovil.ca1>
2026-06-01 17:26:03 -0600

    producto acts

--- resources/views/producto/create.blade.php
+++ resources/views/producto/create.blade.php
@@ -262,10 +262,11 @@ function StopScanner() {
 @push('js')
 <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.14.0-beta3/dist/js/bootstrap-select.min.js"></script>
 <script>
-    $('#btn-generar-codigo').on('click', function(e) {
-    // 💡 ESTA LÍNEA ES VITAL: Detiene el comportamiento de recarga del navegador
+
+$('#btn-generar-codigo').on('click', function(e) {
+    // 💡 ESTO DETIENE EL BLOQUEO DEL LECTOR Y EVITA QUE SE RECARGUE LA PÁGINA
     e.preventDefault(); 
-    e.stopPropagation(); 
+    e.stopPropagation();
 
     $.ajax({
         url: '/ajax-interno/generar-codigo-barras-unico',
